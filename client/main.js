09878728a2a1f962d2328306dc1a5ec151046aac
Max Matthews <max@mzmtech.com>
2016-11-13 09:16:15 -0500

    Last push

--- client/main.js
+++ client/main.js
@@ -1,7 +1,20 @@
 import {Hackathon} from '../lib/models/Hackathons';
 
 Tracker.autorun(function () {
+	const input = $("#hackathonName");
 	const hackathons = Hackathon.find({},{name: 1}).fetch();
 	const hackathonNames = hackathons.map((hackathon)=>{return hackathon.name});
-	$("#hackathonName").typeahead({ source:hackathonNames });
+	input.typeahead({ source:hackathonNames });
+
+	input.change((event)=>{
+		console.log(event.target.value);
+	})
+
+
+});
+
+Template.home.events({
+	'submit form': function(event){
+		event.preventDefault();
+	}
 });
